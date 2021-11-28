558258ca89c485f57eba8101b7fcb133a6cc17d3
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2021-11-28 22:21:32 +0100

    Correct Debug Settings GUI

--- scripts/Gui/DebugSettingsPage.lua
+++ scripts/Gui/DebugSettingsPage.lua
@@ -77,14 +77,230 @@ function ADDebugSettingsPage:updateDebugElements()
 end
 
 function ADDebugSettingsPage:onCreateAutoDriveHeaderText(box)
-    if self.storedHeaderText == nil then
-        self.storedHeaderText = box.text
+    if self.storedHeaderKey == nil then
+        self.storedHeaderKey = box.text
     end
-    if self.storedHeaderText ~= nil then
+    if self.storedHeaderKey ~= nil then
 
-        local hasText = self.storedHeaderText ~= nil and self.storedHeaderText ~= ""
+        local hasText = self.storedHeaderKey ~= nil and self.storedHeaderKey ~= ""
         if hasText then
-            local text = self.storedHeaderText
+            local text = self.storedHeaderKey
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText1(box)
+    if self.storedKey1 == nil then
+        self.storedKey1 = box.text
+    end
+    if self.storedKey1 ~= nil then
+
+        local hasText = self.storedKey1 ~= nil and self.storedKey1 ~= ""
+        if hasText then
+            local text = self.storedKey1
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText2(box)
+    if self.storedKey2 == nil then
+        self.storedKey2 = box.text
+    end
+    if self.storedKey2 ~= nil then
+
+        local hasText = self.storedKey2 ~= nil and self.storedKey2 ~= ""
+        if hasText then
+            local text = self.storedKey2
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText3(box)
+    if self.storedKey3 == nil then
+        self.storedKey3 = box.text
+    end
+    if self.storedKey3 ~= nil then
+
+        local hasText = self.storedKey3 ~= nil and self.storedKey3 ~= ""
+        if hasText then
+            local text = self.storedKey3
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText4(box)
+    if self.storedKey4 == nil then
+        self.storedKey4 = box.text
+    end
+    if self.storedKey4 ~= nil then
+
+        local hasText = self.storedKey4 ~= nil and self.storedKey4 ~= ""
+        if hasText then
+            local text = self.storedKey4
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText5(box)
+    if self.storedKey5 == nil then
+        self.storedKey5 = box.text
+    end
+    if self.storedKey5 ~= nil then
+
+        local hasText = self.storedKey5 ~= nil and self.storedKey5 ~= ""
+        if hasText then
+            local text = self.storedKey5
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText6(box)
+    if self.storedKey6 == nil then
+        self.storedKey6 = box.text
+    end
+    if self.storedKey6 ~= nil then
+
+        local hasText = self.storedKey6 ~= nil and self.storedKey6 ~= ""
+        if hasText then
+            local text = self.storedKey6
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText7(box)
+    if self.storedKey7 == nil then
+        self.storedKey7 = box.text
+    end
+    if self.storedKey7 ~= nil then
+
+        local hasText = self.storedKey7 ~= nil and self.storedKey7 ~= ""
+        if hasText then
+            local text = self.storedKey7
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText8(box)
+    if self.storedKey8 == nil then
+        self.storedKey8 = box.text
+    end
+    if self.storedKey8 ~= nil then
+
+        local hasText = self.storedKey8 ~= nil and self.storedKey8 ~= ""
+        if hasText then
+            local text = self.storedKey8
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText9(box)
+    if self.storedKey9 == nil then
+        self.storedKey9 = box.text
+    end
+    if self.storedKey9 ~= nil then
+
+        local hasText = self.storedKey9 ~= nil and self.storedKey9 ~= ""
+        if hasText then
+            local text = self.storedKey9
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText10(box)
+    if self.storedKey10 == nil then
+        self.storedKey10 = box.text
+    end
+    if self.storedKey10 ~= nil then
+
+        local hasText = self.storedKey10 ~= nil and self.storedKey10 ~= ""
+        if hasText then
+            local text = self.storedKey10
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText11(box)
+    if self.storedKey11 == nil then
+        self.storedKey11 = box.text
+    end
+    if self.storedKey11 ~= nil then
+
+        local hasText = self.storedKey11 ~= nil and self.storedKey11 ~= ""
+        if hasText then
+            local text = self.storedKey11
+            if text:sub(1,6) == "$l10n_" then
+                text = text:sub(7)
+            end
+            text = g_i18n:getText(text)
+            box:setTextInternal(text, false, true)
+        end
+    end
+end
+
+function ADDebugSettingsPage:onCreateAutoDriveText12(box)
+    if self.storedKey12 == nil then
+        self.storedKey12 = box.text
+    end
+    if self.storedKey12 ~= nil then
+
+        local hasText = self.storedKey12 ~= nil and self.storedKey12 ~= ""
+        if hasText then
+            local text = self.storedKey12
             if text:sub(1,6) == "$l10n_" then
                 text = text:sub(7)
             end
@@ -96,5 +312,17 @@ end
 
 function ADDebugSettingsPage:copyAttributes(src)
 	ADDebugSettingsPage:superClass().copyAttributes(self, src)
-    self.storedHeaderText = src.storedHeaderText
+    self.storedHeaderKey = src.storedHeaderKey
+    self.storedKey1 = src.storedKey1
+    self.storedKey2 = src.storedKey2
+    self.storedKey3 = src.storedKey3
+    self.storedKey4 = src.storedKey4
+    self.storedKey5 = src.storedKey5
+    self.storedKey6 = src.storedKey6
+    self.storedKey7 = src.storedKey7
+    self.storedKey8 = src.storedKey8
+    self.storedKey9 = src.storedKey9
+    self.storedKey10 = src.storedKey10
+    self.storedKey11 = src.storedKey11
+    self.storedKey12 = src.storedKey12
 end
