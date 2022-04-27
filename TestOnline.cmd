d00e0ffdd4c328b4b87a0eba5c4da08f1e01c949
Achimobil79 <achimobil@hotmail.com>
2022-04-27 11:05:44 +0200

    Eigene Dateien nach Fork Rücksetzung

---
+++ TestOnline.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\7-Zip\7z.exe" a -tzip "F:\LS22ModTest\mods\FS22_AutoDrive.zip" -x@FileDefinition.txt
+"F:\Program Files (x86)\Farming Simulator 2022\FarmingSimulator2022.exe" -cheats -skipStartVideos -profile F:\LS22ModTest
