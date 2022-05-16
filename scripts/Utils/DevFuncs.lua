f1418c40bf43581c2ee4ac7287ff1c879b65748a
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2022-05-16 17:01:05 +0200

    added debug marker 9_ and console ADCheckDestinations
    ADCheckDestinations check path from current point to all destinations in the network
    without any argument list only missing connection to destination in log.txt
    with something as argument list all connections to destinations

--- scripts/Utils/DevFuncs.lua
+++ scripts/Utils/DevFuncs.lua
@@ -1,3 +1,25 @@
+function AutoDrive:checkDestinations(showAll)
+    local vehicle = g_currentMission.controlledVehicle
+    if vehicle == nil or vehicle.ad == nil or vehicle.ad.stateModule == nil then
+        Logging.error("ADCheckDestinations needs to be called only while entered an AD vehicle")
+        return
+    end
+    local mapMarkers = ADGraphManager:getMapMarkers()
+    if mapMarkers and table.count(mapMarkers) > 0 then
+
+        for index, mapMarker in pairs(mapMarkers) do
+            local wayPoints = ADGraphManager:getPathTo(vehicle, mapMarker.id)
+
+            if wayPoints == nil or (wayPoints[2] == nil and (wayPoints[1] == nil or (wayPoints[1] ~= nil and wayPoints[1].id ~= mapMarker.id))) then
+                Logging.error("[AD] Could not find a path to ->%s<- !", tostring(mapMarker.name))
+            else
+                if showAll then
+                    Logging.info("[AD] Path found to ->%s<- ", tostring(mapMarker.name))
+                end
+            end
+        end
+    end
+end
 function AutoDrive.debugVehicleMsg(vehicle, msg)
     if vehicle ~= nil and vehicle.ad ~= nil and vehicle.ad.debug ~= nil then
         vehicle.ad.debug:Enqueue(msg)
@@ -30,4 +52,5 @@ end
 
 function AutoDrive.devAutoDriveInit()
     Logging.info("[AD] Info: g_server %s g_client %s g_dedicatedServer %s g_dedicatedServerInfo %s getUserProfileAppPath %s getIsClient %s getIsServer %s isMasterUser %s", tostring(g_server), tostring(g_client), tostring(g_dedicatedServer), tostring(g_dedicatedServerInfo), tostring(getUserProfileAppPath()), tostring(g_currentMission:getIsClient()), tostring(g_currentMission:getIsServer()), tostring(g_currentMission.isMasterUser))
+	addConsoleCommand( 'ADCheckDestinations', 'Find path to all destinations', 'checkDestinations', AutoDrive )
 end
